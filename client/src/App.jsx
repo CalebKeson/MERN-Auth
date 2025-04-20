ff92fb3bf40edfa4512ac3edaf9117b23cc69ede
Caleb <lebokeson@gmail.com>
2025-04-20 10:50:16 +0300

    install React and TailwindCSS

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-red-700 text-5xl">
+        Welcome to MERN Authentication
+      </div>
+    </>
+  );
+}
+
+export default App;

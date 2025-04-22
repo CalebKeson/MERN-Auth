4874c77e19858c07ea1f3954611afe0157eb28d5
Caleb <lebokeson@gmail.com>
2025-04-22 16:01:29 +0300

    Create pages and header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,23 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Signin from "./pages/SignIn";
+import Header from "./components/Header";
+import Signup from "./pages/SignUp";
 
 function App() {
   return (
-    <>
-      <div className="text-red-700 text-5xl">
-        Welcome to MERN Authentication
-      </div>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
